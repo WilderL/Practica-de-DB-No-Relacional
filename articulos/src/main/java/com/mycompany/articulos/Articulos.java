3897241c6497fa29edb7a6759320cbd1b5ff2cb8
WilderL <menchuwilder@gmail.com>
2024-10-28 13:09:50 -0600

    Creacion del proyecto

---
+++ articulos/src/main/java/com/mycompany/articulos/Articulos.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.articulos;
+
+/**
+ *
+ * @author WilderL, EAGutierrez04 
+ */
+public class Articulos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
