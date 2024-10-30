c46290b434817759a77276a76ebdd5eca9e9660f
Estuardo Gutiérrez <EAGUTIERREZTZ@correo.url.edu.gt>
2024-10-29 22:52:58 -0600

    Interfaz

--- articulos/src/main/java/com/mycompany/articulos/Articulos.java
+++ articulos/src/main/java/com/mycompany/articulos/Articulos.java
@@ -1,13 +1,18 @@
 
 package com.mycompany.articulos;
+import com.mycompany.articulos.UI.MainWindow;
 
 /**
  *
  * @author WilderL, EAGutierrez04 
  */
 public class Articulos {
+    
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        MainWindow principal = new MainWindow();
+        principal.setVisible(true);
+        
     }
 }
